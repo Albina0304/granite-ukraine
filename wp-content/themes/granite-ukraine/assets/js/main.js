80b91de3c93fe23629260f2a388dacf59dc4a982
Albina0304 <aaa.aniskevich@gmail.com>
2023-08-02 15:38:55 +0200

    footer

--- wp-content/themes/granite-ukraine/assets/js/main.js
+++ wp-content/themes/granite-ukraine/assets/js/main.js
@@ -1,7 +1,7 @@
 //=../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //=../../node_modules/slick-carousel/slick/slick.min.js
 jQuery(document).ready(function ($) {
-    $('.header-wrapper a').click(function(e) {
+    $('.main-wrapper a').click(function(e) {
         e.preventDefault();
         var linkid = $(this).attr('href');
         var headerHeight = $('.header').outerHeight();
@@ -9,8 +9,23 @@ jQuery(document).ready(function ($) {
           scrollTop: $(linkid).offset().top - headerHeight
         }, 1);
     });
-    $('.header-burger, .header-wrapper li a').on ('click', function(e) {
-      $('.header-wrapper').toggleClass('is-active');
+    $('.header-burger, .main-wrapper li a').on ('click', function(e) {
+      $('.main-wrapper').toggleClass('is-active');
       $('#burger-nav').toggleClass('open');
+  });
+  window.onscroll = function() {scrollFunction()};
+  window.onload = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scroll").style.display = "block";
+    } else {
+      document.getElementById("scroll").style.display = "none";
+    }
+  }
+  $('#scroll').click(function() {
+    $('html, body').animate({
+      scrollTop: 0,
+    }, 800);
   });
 });
