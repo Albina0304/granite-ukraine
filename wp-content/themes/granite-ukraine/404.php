4f3a43b1c5e6f3731f064ce13ee2dc40e4fb853c
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-13 18:20:01 +0200

    section 404

---
+++ wp-content/themes/granite-ukraine/404.php
@@ -0,0 +1,9 @@
+<?php get_header()?>
+<div class="section section-404">
+    <div class="container">
+        <h1>
+            404<br>not found!
+        </h1>
+    </div>
+</div>
+<?php get_footer()?>
