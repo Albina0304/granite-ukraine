661328322b02604975de487472d0c7f9b52dd5f5
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-22 20:07:55 +0200

    advantages section optimization

--- wp-content/themes/granite-ukraine/template-parts/modules/advantages.php
+++ wp-content/themes/granite-ukraine/template-parts/modules/advantages.php
@@ -1,42 +1,41 @@
 <?php 
 $title = get_sub_field('title');
 $repeater_cards = get_sub_field('repeater_card');
-if('advantages') :
-?>
-<section class="advantages decor-light section">
-    <div class="container">
-        <?php if ($title):?>
-            <h2 class="advantage-title">
-                <?php echo $title;?>
-            </h2>
-        <?php endif;?>
-        <div class="advantage-colons">
-        <?php
-            if ($repeater_cards) {
-                foreach ($repeater_cards as $card) {
-                    $image = $card['image'];
+if($repeater_cards) : ?>
+    <section class="section advantages">
+        <div class="decor-light">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/declight.png';?>" alt="" loading="lazy">
+        </div>
+        <div class="container">
+            <?php if ($title):?>
+                <h2 class="advantage-title">
+                    <?php echo $title;?>
+                </h2>
+            <?php endif;?>
+            <div class="advantage-colons">
+                <?php foreach ($repeater_cards as $card) :
+                    $image = isset($card['image']) ? $card['image'] : '';
                     $title_card = $card['title'];
                     $description = $card['description'];?>
                     <div class="advantage-card">
-                        <?php if ($image) : ?>
-                            <div class="image-card">
+                        <?php if (isset ($image['ID'])) : ?>
+                            <div class="advantage-image">
                                 <?php echo wp_get_attachment_image($image['ID'], 'card-img'); ?>
                             </div>
                         <?php endif; ?>
                         <?php if ($title_card) : ?>
-                            <h3 class="title-card">
+                            <h3 class="advantage-card-title">
                                 <?php echo $title_card; ?>
                             </h3>
                         <?php endif; ?>
                         <?php if ($description) : ?>
-                            <div class="description-card">
+                            <div class="advantage-description">
                                 <?php echo $description; ?>
                             </div>
                         <?php endif; ?>
                     </div>
-                <?php 
-            }}?>
+                <?php endforeach;?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php endif;?>
