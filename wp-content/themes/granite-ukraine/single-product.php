b205cfb1211da866841e14ab6626bec3e20d5176
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-22 20:01:29 +0200

    single-product section optimization

--- wp-content/themes/granite-ukraine/single-product.php
+++ wp-content/themes/granite-ukraine/single-product.php
@@ -5,13 +5,26 @@ $product_images = get_field('product_images');
 $slider_images = get_field('slider_images');
 $post_id = get_the_ID();
 $link = get_field('btn_primary', 'options');
-?>
-<section class="section decor-light hero-product">
+$content = get_the_content();
+$title = get_the_title();
+$related_products = get_posts(
+    array(
+        'post_type' => 'product',
+        'posts_per_page' => 4,
+        'suppress_filters' => false,
+        'post__not_in' => array($post_id),
+        'orderby' => 'rand'
+    )
+);?>
+<section class="section hero-product">
+    <div class="decor-light">
+        <img src="<?php echo get_template_directory_uri().'/assets/images/declight.png';?>" alt="" loading="lazy">
+    </div>
     <div class="container">
         <div class="hero-card">
             <div class="hero-slider-wrap">
-                <div class="card-image">
-                    <div class="slider-image">
+                <div class="slider-image">
+                    <div class="slider-image-card">
                         <a href="<?php echo get_the_post_thumbnail_url();?>" data-lightbox="hero-granite">
                             <?php the_post_thumbnail('product-card');?>
                         </a>
@@ -19,21 +32,24 @@ $link = get_field('btn_primary', 'options');
                     <?php 
                     if ($slider_images) :
                         foreach ($slider_images as $slider_image) : 
-                            $image_id = $slider_image['image']['ID'];?>
-                            <div class="slider-image">
-                                <?php echo wp_get_attachment_image($image_id, 'product-card');?>
+                            $image_id = $slider_image['image']['ID'];
+                            $image_large = wp_get_attachment_image_src($image_id, 'product-card');?>
+                            <div class="slider-image-card">
+                                <a href="<?php echo $image_large[0];?>" data-lightbox="hero-granite">
+                                    <?php echo wp_get_attachment_image($image_id, 'product-card');?>
+                                </a>
                             </div> 
                         <?php endforeach;
                     endif; ?>
                 </div>
                 <div class="hero-slider">
-                    <div class="slider-image">
+                    <div class="slider-image-card">
                         <?php the_post_thumbnail('product-card', array('data-lightbox' => 'hero-granite'));?>
                     </div>
                     <?php if ($slider_images) :
                         foreach ($slider_images as $slider_image) : 
                             $image_id = $slider_image['image']['ID']; ?>
-                            <div class="slider-image">
+                            <div class="slider-image-card">
                                     <?php echo wp_get_attachment_image($image_id, 'slider-img');?>
                             </div> 
                         <?php endforeach; 
@@ -41,77 +57,68 @@ $link = get_field('btn_primary', 'options');
                 </div>
             </div>
             <div class="hero-info">
-                <?php if (the_title()) : ?>
-                    <h3 class="card-title">
-                        <?php the_title();?>
+                <?php if ($title) : ?>
+                    <h3 class="hero-info-title">
+                        <?php echo $title;?>
                     </h3>
                 <?php endif;?>
-                <?php if (the_content()) : ?>
-                    <div class="card-description">
-                        <?php the_content();?>
+                <?php if ($content) : ?>
+                    <div class="hero-info-description">
+                        <?php echo $content;?>
                     </div>
                 <?php endif;?>
                 <?php if ($price_from) : ?>
-                    <div class="price-from">
+                    <div class="hero-info-price">
                         <?php echo $price_from;?>
                     </div>
-                <?php endif;?>
-                <div class="section-button">
-                    <?php 
-                    if( $link ): 
-                        $link_target = $link['target'] ? $link['target'] : '_self';?>
-                        <a type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-price" href="#">
+                <?php endif;
+                if($link) : ?>
+                    <div class="section-button">
+                        <?php $link_target = $link['target'] ? $link['target'] : '_self';?>
+                        <a href="#" type="button" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-price">
                             <?php echo __('Прайс', 'granite-ukraine'); ?>
                         </a>
-                    <?php endif; ?>
-                </div>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </section>
-<section class="labor">
-    <?php 
-        $arr = array(
+<?php if($product_images) : ?>
+    <section class="labor">
+        <?php $arr = array(
             'title' => __('Роботи', 'granite-ukraine'),
             'images' => array_slice($product_images, 0, 6),
             'link' => array(
                 'title' => __('Завантажити більше', 'granite-ukraine')
             )
         );
-        echo get_template_part('template-parts/modules/labors', '', $arr);
-    ?>
-</section>
-<section class="related-products products decor-light section">
-    <div class="container">
-        <?php
-        $related_products = get_posts(
-            array(
-                'post_type' => 'product',
-                'posts_per_page' => 4,
-                'suppress_filters' => false,
-                'post__not_in' => array($post_id),
-                'orderby' => 'rand'
-            )
-        );
-        if (isset($title)):?>
-            <h2 class="products-title">
-                <?php echo $title; ?>
-            </h2>
-        <?php endif;?>
-        <div class="product-cards">
-            <?php if ($related_products) {
-                foreach($related_products as $post) {
+        echo get_template_part('template-parts/modules/labors', '', $arr);?>
+    </section>
+<?php endif;
+if($related_products) : ?>
+    <section class="section products related-products">
+        <div class="decor-light">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/declight.png';?>" alt="" loading="lazy">
+        </div>
+        <div class="container">
+            <?php if (isset($title)):?>
+                <h2 class="products-title">
+                    <?php echo $title; ?>
+                </h2>
+            <?php endif;?>
+            <div class="product-cards">
+                <?php foreach($related_products as $post) :
                     setup_postdata($post);?>
                     <div class="product-col">
                         <?php echo get_template_part('template-parts/cards/product-card');?>
                     </div>
-                <?php }
-            wp_reset_postdata(); }?>
+                <?php endforeach;
+                wp_reset_postdata(); ?>
+            </div>
         </div>
-    </div>
-    </div>
-</section>
-<?php     
-echo get_template_part('template-parts/modules/form');
-echo get_template_part('template-parts/modules/map');?>
-<?php get_footer();
+        </div>
+    </section>
+<?php endif;
+    echo get_template_part('template-parts/general/modal-prices');
+get_footer();
--- wp-content/themes/granite-ukraine/template-parts/cards/product-card.php
+++ wp-content/themes/granite-ukraine/template-parts/cards/product-card.php
@@ -1,10 +1,12 @@
 <a href="<?php echo get_permalink();?>" class="product-card">
-    <div class="card-image">
-        <?php the_post_thumbnail('product-img'); ?>
-    </div>
-    <div class="card-title">
-        <h4>
+    <?php if(the_post_thumbnail('product-img')) : ?>
+        <div class="card-image">
+            <?php the_post_thumbnail('product-img'); ?>
+        </div>
+    <?php endif;
+    if(get_the_title()) : ?>
+        <h4 class="card-title">
             <?php the_title();?>
         </h4>
-    </div>
+    <?php endif;?>
 </a>
