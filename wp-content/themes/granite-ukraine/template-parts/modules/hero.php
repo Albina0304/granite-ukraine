208b04d2617d7027dd005ed307d96969f8431cc1
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-22 19:58:44 +0200

    hero section optimization

--- wp-content/themes/granite-ukraine/template-parts/modules/hero.php
+++ wp-content/themes/granite-ukraine/template-parts/modules/hero.php
@@ -1,22 +1,20 @@
 <?php
 $text = get_sub_field('text');
 $link = get_field('btn_primary', 'options');
-?>
-<section class="hero decor">
-    <div class="container">
-        <?php if ($text):?>
+if($text) : ?>
+    <section class="decor hero">
+        <div class="container">
             <h1 class="hero-title">
                 <?php echo $text;?>
             </h1>
-        <?php endif;?>
-        <div class="section-button">
-            <?php 
-            if( $link ): 
-                $link_target = $link['target'] ? $link['target'] : '_self';?>
-                <a type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-form" href="#">
-                    <?php echo esc_html( $link['title'] ); ?>
-                </a>
+            <?php if( $link ): ?>
+                <div class="section-button">
+                    <?php $link_target = $link['target'] ? $link['target'] : '_self';?>
+                    <a href="#" type="button" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-form">
+                        <?php echo esc_html( $link['title'] ); ?>
+                    </a>
+                </div>
             <?php endif; ?>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif;
