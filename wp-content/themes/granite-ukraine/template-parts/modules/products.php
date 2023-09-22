2c6137a37ed79e97c1b07bdb3fae410b4f47e914
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-22 19:55:57 +0200

    Fixed bugs in the section products

--- wp-content/themes/granite-ukraine/template-parts/modules/products.php
+++ wp-content/themes/granite-ukraine/template-parts/modules/products.php
@@ -6,28 +6,31 @@ $products = get_posts(
         'posts_per_page' => -1,
         'suppress_filters' => false
     )
-);?>
-<section class="products decor-light section">
-    <div class="container">
-        <?php if ($title):?>
-            <h2 class="products-title">
-                <?php echo $title;?>
-            </h2>
-        <?php endif;?>
-        <div class="product-cards">
-            <?php
-            if ($products) {
-                foreach ($products as $post) {
-                    setup_postdata($post);?>
-                    <div class="product-col">
-                        <?php echo get_template_part('template-parts/cards/product-card');?>
-                    </div>
-                <?php }
-            wp_reset_postdata(); }
-            ?>
+);
+if($products) : ?>
+    <section class="section products">
+        <div class="decor-light">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/declight.png';?>" alt="" loading="lazy">
         </div>
-    </div>
-</section>
+        <div class="container">
+            <?php if ($title):?>
+                <h2 class="products-title">
+                    <?php echo $title;?>
+                </h2>
+            <?php endif;?>
+            <div class="product-cards">
+                <?php 
+                    foreach ($products as $post) :
+                        setup_postdata($post);?>
+                        <div class="product-cards-column">
+                            <?php echo get_template_part('template-parts/cards/product-card');?>
+                        </div>
+                    <?php endforeach;
+                wp_reset_postdata();?>
+            </div>
+        </div>
+    </section>
+<?php endif;
 
 
 
