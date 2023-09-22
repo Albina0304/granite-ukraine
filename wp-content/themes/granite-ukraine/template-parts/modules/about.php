ee2c3645da0181b87ee6c459ea0d0ed6ac809640
Albina0304 <aaa.aniskevich@gmail.com>
2023-09-22 19:59:11 +0200

    about section optimization

--- wp-content/themes/granite-ukraine/template-parts/modules/about.php
+++ wp-content/themes/granite-ukraine/template-parts/modules/about.php
@@ -3,42 +3,44 @@ $title = get_sub_field('title');
 $description = get_sub_field('description');
 $repeater = get_sub_field('repeater');
 $link = get_field('btn_primary', 'options');
-if('about') :
-?>
-<section class="section about decor-light">
-    <div class="container">
-        <?php if ($title):?>
-            <h2 class="about-title">
-                <?php echo $title;?>
-            </h2>
-        <?php endif;?>
-        <div class="about-columns">
-            <div class="column">
-                <?php echo $description;?>
-            </div>
-            <div class="column">
-                <?php
-                if ($repeater) {
-                    foreach ($repeater as $img) {
-                        $image = $img['image'];?>
-                        <?php if ($image) : ?>
-                            <div class="column-image">
-                                <?php echo wp_get_attachment_image($image['ID'], 'about-image'); ?>  
-                            <?php endif; ?>
-                        </div>
-                    <?php 
-                }}?>
-            </div>
+if($repeater && $description) : ?>
+    <section class="section about">
+        <div class="decor-light">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/declight.png';?>" alt="" loading="lazy">
         </div>
-        <div class="section-button">
-            <?php 
-            if( $link ): 
-                $link_target = $link['target'] ? $link['target'] : '_self';?>
-                <a type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-form" href="#">
-                    <?php echo esc_html( $link['title'] ); ?>
-                </a>
+        <div class="container">
+            <?php if ($title) : ?>
+                <h2 class="about-title">
+                    <?php echo $title;?>
+                </h2>
+            <?php endif;?>
+            <div class="about-columns">
+                <?php if ($description) : ?>
+                    <div class="about-column">
+                        <?php echo $description;?>
+                    </div>
+                <?php endif;
+                if ($repeater) : ?>
+                    <div class="about-column">
+                        <?php foreach ($repeater as $img) :
+                            $image = isset($img['image']) ? $img['image'] : '';
+                            if (isset($image['ID'])) : ?>
+                                <div class="about-column-image">
+                                    <?php echo wp_get_attachment_image($image['ID'], 'about-image'); ?>  
+                                </div>
+                            <?php endif;
+                        endforeach;?>
+                    </div>
+                <?php endif;?>    
+            </div>
+            <?php if( $link ): ?>
+                <div class="section-button">
+                    <?php $link_target = $link['target'] ? $link['target'] : '_self';?>
+                    <a href="#" type="button" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-form">
+                        <?php echo esc_html( $link['title'] ); ?>
+                    </a>
+                </div>
             <?php endif; ?>
         </div>
-    </div>
-</section>
-<?php endif;?>
+    </section>
+<?php endif;
