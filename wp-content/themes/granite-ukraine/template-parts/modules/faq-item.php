b0ffe6a3039c21224066e3ac41ddff1a72c2533b
Albina <aaa.aniskevich@gmail.com>
2024-02-21 11:38:30 +0100

    Fixed errors on the faqs page

--- wp-content/themes/granite-ukraine/template-parts/modules/faq-item.php
+++ wp-content/themes/granite-ukraine/template-parts/modules/faq-item.php
@@ -1,24 +1,26 @@
 <?php extract($args);?>
 <div class="accordion accordion-main" id="accordion-open">
     <?php if (is_array($faqs) && !empty($faqs)) {
-        foreach ($faqs as $faq => $post_id) :
-            $postQuestions = get_post($post_id);
-            if ($postQuestions) {?>
+        foreach ($faqs as $key => $faqPost) :
+            $post = get_post($faqPost);
+            setup_postdata($post);
+            if ($post) {?>
                 <div class="accordion-item">
-                    <h2 class="accordion-title" id="accordion-heading-<?php echo $faq; ?>">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?php echo $faq; ?>" aria-expanded="false" aria-controls="collapse-<?php echo $faq; ?>">
-                            <?php echo esc_html($postQuestions->post_title); ?>
+                    <h2 class="accordion-title" id="accordion-heading-<?php echo get_the_ID();?>">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?php echo get_the_ID(); ?>" aria-expanded="false" aria-controls="collapse-<?php echo get_the_ID(); ?>">
+                            <?php the_title(); ?>
                             <div class="accordion-button-img">
                                 <div class="accordion-button-img-open"></div> 
                                 <div class="accordion-button-img-close"></div> 
                             </div>
                         </button>
                     </h2>
-                    <div id="collapse-<?php echo $faq; ?>" class="accordion-collapse collapse" aria-labelledby="accordion-heading-<?php echo $faq; ?>" data-bs-parent="#accordion-open">
-                        <div class="accordion-body"><?php echo apply_filters('the_content', $postQuestions->post_content); ?></div>
+                    <div id="collapse-<?php echo get_the_ID(); ?>" class="accordion-collapse collapse" aria-labelledby="accordion-heading-<?php echo get_the_ID(); ?>" data-bs-parent="#accordion-open">
+                        <div class="accordion-body"><?php the_content(); ?></div>
                     </div>
                 </div>
             <?php }
         endforeach;
+        wp_reset_postdata();
     };?>
 </div>
