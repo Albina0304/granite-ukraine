ba8e8dcb2399c1c5b53e5ac4e8b14a0d4a22406b
Albina <aaa.aniskevich@gmail.com>
2024-02-28 11:02:53 +0100

    feat: Added container to blog-page

--- wp-content/themes/granite-ukraine/single.php
+++ wp-content/themes/granite-ukraine/single.php
@@ -1,13 +1,15 @@
 <!-- Внутри одного поста -->
         <?php get_header();?>
-        <div class="section blog-page blog-description">
-            <?php if(has_post_thumbnail()):
-                the_post_thumbnail(get_the_ID());
-            endif;?>
-            <h2>
-                <?php the_title();?>
-            </h2>
-            <?php the_content();?>
+        <div class="container">
+            <div class="section blog-page blog-description">
+                <?php if(has_post_thumbnail()):
+                    the_post_thumbnail(get_the_ID());
+                endif;?>
+                <h2>
+                    <?php the_title();?>
+                </h2>
+                <?php the_content();?>
+            </div>
         </div>
     </div>
 </div>
